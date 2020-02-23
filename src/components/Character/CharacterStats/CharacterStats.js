8b38f0507170826e129292ea7e267261f2676245
moonbyt3 <moonbyt3@gmail.com>
2020-02-23 23:46:36 +0100

    fixed stats modifier inputs

--- src/components/Character/CharacterStats/CharacterStats.js
+++ src/components/Character/CharacterStats/CharacterStats.js
@@ -8,27 +8,33 @@ export default function CharacterStats() {
         <div className="character-stats">
             <div className="character-stats__item">
                 <label htmlFor="" className="character-stats__item-label">Strength</label>
-                <input type="text" className="character-stats__item-input"/>
+                <input type="text" className="character-stats__item-input-ability-modifier"/>
+                <input type="text" className="character-stats__item-input-ability-score"/>
             </div>
             <div className="character-stats__item">
                 <label htmlFor="" className="character-stats__item-label">Dexterity</label>
-                <input type="text" className="character-stats__item-input"/>
+                <input type="text" className="character-stats__item-input-ability-modifier"/>
+                <input type="text" className="character-stats__item-input-ability-score"/>
             </div>
             <div className="character-stats__item">
                 <label htmlFor="" className="character-stats__item-label">Constitution</label>
-                <input type="text" className="character-stats__item-input"/>
+                <input type="text" className="character-stats__item-input-ability-modifier"/>
+                <input type="text" className="character-stats__item-input-ability-score"/>
             </div>
             <div className="character-stats__item">
                 <label htmlFor="" className="character-stats__item-label">Inteligence</label>
-                <input type="text" className="character-stats__item-input"/>
+                <input type="text" className="character-stats__item-input-ability-modifier"/>
+                <input type="text" className="character-stats__item-input-ability-score"/>
             </div>
             <div className="character-stats__item">
                 <label htmlFor="" className="character-stats__item-label">Wisdom</label>
-                <input type="text" className="character-stats__item-input"/>
+                <input type="text" className="character-stats__item-input-ability-modifier"/>
+                <input type="text" className="character-stats__item-input-ability-score"/>
             </div>
             <div className="character-stats__item">
                 <label htmlFor="" className="character-stats__item-label">Charisma</label>
-                <input type="text" className="character-stats__item-input"/>
+                <input type="text" className="character-stats__item-input-ability-modifier"/>
+                <input type="text" className="character-stats__item-input-ability-score"/>
             </div>
         </div>
     )
