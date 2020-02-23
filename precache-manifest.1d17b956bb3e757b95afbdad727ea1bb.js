e264a078a7a15f34709b2738da948f711d175255
Milorad <miloradjekic@outlook.com>
2020-02-23 19:34:40 +0100

    Updates

---
+++ precache-manifest.1d17b956bb3e757b95afbdad727ea1bb.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4de7734ac13d33d18db47c520bbefc2a",
+    "url": "/dnd-character/index.html"
+  },
+  {
+    "revision": "0270397a74be27bc158d",
+    "url": "/dnd-character/static/css/main.bc597b2e.chunk.css"
+  },
+  {
+    "revision": "40855737c84c214349d3",
+    "url": "/dnd-character/static/js/2.dec5b92c.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/dnd-character/static/js/2.dec5b92c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0270397a74be27bc158d",
+    "url": "/dnd-character/static/js/main.09b290d6.chunk.js"
+  },
+  {
+    "revision": "9590250f5bf1fcfc73ae",
+    "url": "/dnd-character/static/js/runtime-main.65fb8cb0.js"
+  },
+  {
+    "revision": "ae7636d55943237bb6b9661f9baed220",
+    "url": "/dnd-character/static/media/headerChar.ae7636d5.svg"
+  },
+  {
+    "revision": "839de54607689774f19128c353b35fd8",
+    "url": "/dnd-character/static/media/headerInfo.839de546.svg"
+  },
+  {
+    "revision": "b6de31878061a5c4eaefea5b542c581a",
+    "url": "/dnd-character/static/media/inspiration.b6de3187.svg"
+  },
+  {
+    "revision": "6b70027d94efe0e06a5d316e18c774c8",
+    "url": "/dnd-character/static/media/stat.6b70027d.svg"
+  }
+]);
